39078b07df7821b04b321873cf2240753174e4d4
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2024-05-11 23:14:16 +0900

    change title again

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import random
 
 def main():
-    st.title("Jinnichan's Food Recommender")
+    st.title("Food Recommender")
 
     # Text input
     user_input = st.text_input("sawadee krap! what is your name:")
